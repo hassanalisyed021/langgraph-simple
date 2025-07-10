9623996c7cd7d46f56da645ac7eeb8017e0e8a40
Syed Hassan Ali <hassanalisyed021@gmail.com>
2025-07-11 04:47:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain_ollama import ChatOllama
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 llm = ChatOllama(model="llama3.2:1b")
 
